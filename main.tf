32a1e8d31ced3f954140fc38e3b1adbd6b190cc6
surendrahari09 <115442378+surendrahari09@users.noreply.github.com>
2022-10-31 16:18:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,3 @@
-resource "aws_vpc" "Main" {
-  cidr_block       = var.main_vpc_cidr
-  instance_tenancy = "default"
-}
-
-resource "aws_internet_gateway" "IGW" {
-   vpc_id =  aws_vpc.Main.id
-}
-
-resource "aws_subnet" "publicsubnets" {
-  vpc_id =  aws_vpc.Main.id
-  cidr_block = var.public_subnets
-}
-
-resource "aws_route_table" "PublicRT" {
-   vpc_id =  aws_vpc.Main.id
-   route {
-     cidr_block = "0.0.0.0/0"
-     gateway_id = aws_internet_gateway.IGW.id
-    }
-}
-
-resource "aws_route_table_association" "PublicRTassociation" {
-   subnet_id = aws_subnet.publicsubnets.id
-   route_table_id = aws_route_table.PublicRT.id
-}
 
 resource "aws_instance" "my-machine" {
   ami = var.ami
