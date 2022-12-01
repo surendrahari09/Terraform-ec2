3b74460ec2077e9b46fb7984c6705e6926ee2218
Surendra Hari <115442378+surendrahari09@users.noreply.github.com>
2022-12-01 12:29:49 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,3 @@
-output "ec2_arn" {
-   value = aws_instance.my-machine.arn
-}
-output "instance_ip_addr" {
-  value       = aws_instance.my-machine.public_ip
-  description = "The public IP address of the main server instance."
+provider "aws" {
+    region = "ap-south-1"	
 }
