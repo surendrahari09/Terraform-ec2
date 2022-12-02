ab95d49a3cacbaf9df2dec58df69dcd23b588f60
Surendra Hari <115442378+surendrahari09@users.noreply.github.com>
2022-12-02 12:48:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@
 resource "aws_instance" "my-machine" {
   ami = var.ami
   instance_type = var.instance_type
-   subnet_id     = "subnet-06de6c089b7d56a9b"
+  subnet_id     = "subnet-06de6c089b7d56a9b"
+  key_name = var.key_name
   tags = {
     Name = "my-ec2-machine"
   }
