f16c55abc6fc77b73aef42154d92e6f33986606d
Surendra Hari <115442378+surendrahari09@users.noreply.github.com>
2022-12-02 11:57:55 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    access_key = "AKIA5FIOCDDBSAHM2FIE"
-    secret_key = "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"
+    access_key = "AKIA5FIOCDDBT4YJLCDE"
+    secret_key = "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"
     region = "ap-south-1"	
 }
